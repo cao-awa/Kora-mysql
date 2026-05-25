692e6baa13b32dcf5ff3d2631504a912ed4547df
cao_awa <cao.awa.www@gmail.com>
2026-05-25 15:38:22 +0800

    ...

--- src/main/kotlin/com/github/cao/awa/kora/mysql/data/line/EmptyLine.kt
+++ src/main/kotlin/com/github/cao/awa/kora/mysql/data/line/EmptyLine.kt
@@ -1,9 +1,3 @@
 package com.github.cao.awa.kora.mysql.data.line
 
-import com.github.cao.awa.kora.mysql.data.Column
-
-object EmptyLine: Line() {
-    override fun add(column: Column, lineData: String?) {
-        throw UnsupportedOperationException("Cannot add data to empty line")
-    }
-}
+object EmptyLine: Line({})
